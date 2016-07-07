3bc8d8b1406ea55bd569081086eb628f11066694
aroger <aroger@rapid7.com>
2016-07-07 17:45:51 +0100

    updates

---
+++ cpuState.sh
@@ -0,0 +1,3 @@
+    sudo cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
+    sudo cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq;
+    sudo cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq;
